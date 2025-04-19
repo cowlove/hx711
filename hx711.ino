70453d46dff6b8024e64466f2d1453fbc2af2f4a
jim@vheavy.com <jim@vheavy.com>
2025-04-19 15:13:25 -0700

    Working sample code with digital scale

--- hx711.ino
+++ hx711.ino
@@ -1,21 +1,37 @@
 #include "jimlib.h"
 #include "serialLog.h"
 #ifndef CSIM
-#include "rom/uart.h"
+#include "Adafruit_HX711.h"
 #endif
+Adafruit_HX711 hx(23,22);
+
 
 JStuff j;
 CLI_VARIABLE_FLOAT(x, 1);
 
 void setup() {
+    hx.begin();
     j.begin();
     j.cli.on("RESET", [](){ ESP.restart(); });
 }
 
 void loop() {
     j.run();
-    OUT("loop %f", (float)x);
-    delay(1000);
+    pinMode(19, OUTPUT);
+    digitalWrite(19, 1);
+    delay(1);
+    hx.powerDown(true);
+    delay(1);
+    hx.powerDown(false); 
+    delay(100); 
+    float sum = 0.0;
+    int reads = 20;
+    for (int i = 0; i < reads; i++) 
+         sum += hx.readChannelRaw();
+    sum /= reads;
+    digitalWrite(19, 0);
+    OUT("%.2f", sum);
+    delay(1);
 }
 
 #ifdef CSIM
